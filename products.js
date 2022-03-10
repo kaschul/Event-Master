cbd30887d5f56ebf393b50ce572abf3b063e46e5
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-10 12:02:02 -0800

    products list created

---
+++ products.js
@@ -0,0 +1,59 @@
+const products = [
+
+    {
+        __id: '1',
+        name: 'Events after Dark - Bodies and Bones',
+        organizer: 'Slater Museum of Natural History',
+        image: '',
+        description: "Join us for an insider look at the research and teaching specimins that make up the Slater Museum of Natural History.  Each event features a subset of the collection, including touchable artifacts.  Space is limited, please reserve your ticket in advance.",
+        date: 'March 27th, 2022',
+        location: ['University of Puget Sound', '1500 N Warner St', 'Tacoma, WA 98416', 'Thompson Hall room 295'],
+        time: '5pm - 9pm',
+        price: 10,
+        ticketsStock: 50,
+        categories: ['family friendly', 'science', 'museum', 'Tacoma, WA']
+        },
+
+    {
+        __id: '2',
+        name: 'Events after Dark - Creatures of the Sea',
+        organizer: 'Slater Museum of Natural History',
+        image: '',
+        description: "Join us for an insider look at the research and teaching specimins that make up the Slater Museum of Natural History.  Each event features a subset of the collection, including touchable artifacts.  Space is limited, please reserve your ticket in advance.",
+        date: 'April 17th, 2022',
+        time: '5pm - 19pm',
+        location: ['University of Puget Sound', '1500 N Warner St', 'Tacoma, WA 98416', 'Thompson Hall room 295'],
+        price: 10,
+        ticketsStock: 50,
+        categories: ['family friendly', 'science', 'museum', 'Tacoma, WA']
+    },
+
+    {
+        _id: '3',
+        name: 'Drinks and Discussions - Art and Ownership',
+        organizer: 'The Society of Indigenous Artists',
+        image: '',
+        description: "Chat and mingle at this black tie private event at the Burke Museum.  Join the silent auction and meet local artists.  Dinner and drinks are included in ticket price, admissions are 21+ only.  All proceeds support The Soceity of Indigenous Artists.",
+        date: 'March 23rd, 2022',
+        time: '6pm - 11pm',
+        location: ['Burke Museum', '4300 15th Ave Ne', 'Seattle, WA'],
+        price: 500,
+        ticketsStock: 200,
+        categories: ['art', 'museum', 'fundraiser', 'Seattle, WA', '21+']
+    },
+
+    {
+        _id: '4',
+        name: 'The Wednesday Comedy Show',
+        organizer: 'Seattle City Improv',
+        image: '',
+        description: "Located at Pike Place Market, Seattle City Improv is consistently voted one of the top comedy clubs in Seattle.",
+        date: 'March 23, 2022',
+        time: '1pm',
+        location: ['Pike Place Market', '85 Pike St', 'Seattle, WA'],
+        price: 20,
+        ticketsStock: 80,
+        categories: ['comedy', '21+', 'Seattle, WA']
+    }
+
+]
