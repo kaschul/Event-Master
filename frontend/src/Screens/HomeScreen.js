1732f6fab0c4dcc28f9108feffd28660a0b308c8
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-03-14 09:10:01 -0700

    mapping

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -8,16 +8,19 @@ const HomeScreen = () => {
     <>
       <h1> Events </h1>
       <Row>
-        {products.map((product) => (                                  /* bind array of prod to html. map is for loop "for each product" lets call it product*/
+        {products.map((product) => (                                  /* bind array of prod to html. map is for loop "for each product" lets call output product which is the map variable.*/
 
-          <Col key={product._id} sm={12} md={6} lg={4} lx={3}>          /* in each row product displayed as a column*/
-            <Product product={product} />
+          <Col key={product._id} sm={12} md={6} lg={4} lx={3}>         
+            <Product product={product} /> 
           </Col>
-      
-      ))}
+                                                                      /* two statements above: in Product comp, the product parameter comes from above output "product" which is the map variable*/
+        ))}
       </Row>
-    </>
+    </> 
   )
   }
 
 export default HomeScreen
+
+
+
