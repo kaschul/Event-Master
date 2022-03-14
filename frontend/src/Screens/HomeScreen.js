77df1e4a8848b18d8780de358f7e49635e4629bd
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-03-14 08:49:07 -0700

    setup homescreen

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Product from '../components/Product'
+import products from '../products'
+import {Row, Col} from 'react-bootstrap'
 
 const HomeScreen = () => {
   return (
-    <div><Product /></div>
+    <>
+      <h1> Events </h1>
+      <Row>
+        {products.map((product) => (                                  /* bind array of prod to html. map is for loop "for each product" lets call it product*/
+
+          <Col key={product._id} sm={12} md={6} lg={4} lx={3}>          /* in each row product displayed as a column*/
+            <Product product={product} />
+          </Col>
+      
+      ))}
+      </Row>
+    </>
   )
-}
+  }
 
 export default HomeScreen
