5aa36ff8408f8652347d74613a9ae0ece4a9c483
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-14 14:22:36 -0700

    removed import product

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import products from '../products'
 
 
 
