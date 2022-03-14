c5586bcba586ba9ce2b8a0ba04ee2e3733906e0e
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-14 14:59:55 -0700

    added hook to productscreen

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -13,7 +13,7 @@ const HomeScreen = () => {
       setProducts(data)
     }
     fetchProducts()
-  })
+  }, [])
 
   return (
     <>
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react' //k: added useState, useEffect
+import axios from 'axios' //k: added axios
 import {Link} from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 
 
 
 const ProductScreen = () => {
+  const params = useParams(); //k: added useState, useEffect, axios - hook
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/products/${params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
+
   return (
     <div>ProductScreen</div>
   )
