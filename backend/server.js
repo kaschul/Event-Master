3b1ec45d931ee9e254c0dc57272eae44263e3ace
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-15 15:00:03 -0700

    port changed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 //k: changed import statements for type:module
 import express from 'express'
 import products from './data/products.js'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 
 app.get('/api/products', (req, res) => {
     res.json(products)
@@ -13,4 +15,5 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 })
 
-app.listen(5000, console.log('Server is running on port 5000'));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
