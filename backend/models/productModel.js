9f9f31fa7a60203277e3bde1603c7f43124d5707
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-21 20:51:58 -0700

    added user model, product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    organizer: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    ticketsStock: {
+        type: Number,
+        required: true
+    },
+    categories: {
+        type: Array,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Products = mongoose.model('Products', productSchema)
+export default Products
---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+
+const Users = mongoose.model('Users', userSchema)
+export default Users
