943b6cf9ffcc2949b63e03d25151e5db5087b006
jessica <jessica.vargas@headlight.com>
2022-03-23 23:16:25 -0700

    errormiddleware.js

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
 app.use('/api/products', productRoutes)
-
+app.use(errorHandler)
+app.use(notFound)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
