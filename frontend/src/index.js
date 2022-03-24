55a3d46755f84014e11ddd219f85b4ef9bcadd99
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-24 12:08:08 -0700

    bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './bootstrap1.css'
+import './bootstrap.css'
 
 
 ReactDOM.render(
