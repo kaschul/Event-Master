2adda875a6d70e8e47731fc3946f036a5fc77379
kaschul <47723396+kaschul@users.noreply.github.com>
2022-03-30 16:48:29 -0700

    update homescreen

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import Product from '../components/Product'
+import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
-import axios from 'axios'
+
+import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProducts } from '../actions/productActions'
+
+
 
 const HomeScreen = () => {
 
-  // hook
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const {loading, error, products} = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1> Events </h1>
-      <Row>
+      {loading ? (<Loader />) : error ? (<Message variant='danger'>{error}</Message>) :
+      (<Row>
         {products.map((product) => (                                  /* bind array of prod to html. map is for loop "for each product" lets call output product which is the map variable.*/
 
           <Col key={product._id} sm={12} md={6} lg={4} lx={3}>         
@@ -27,10 +31,11 @@ const HomeScreen = () => {
           </Col>
                                                                       /* two statements above: in Product comp, the product parameter comes from above output "product" which is the map variable*/
         ))}
-      </Row>
+      </Row>)}
     </> 
   )
-  }
+  
+}
 
 export default HomeScreen
 
