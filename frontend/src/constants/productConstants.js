53c0599bc4a072bdc40e3358c2f363abcbbd8504
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-03-30 17:56:21 -0700

    productReducers added

--- frontend/src/constants/productConstants.js
+++ frontend/src/constants/productConstants.js
@@ -4,10 +4,10 @@
  /* These three req. pertain to acquiring the product list as a whole */
 export const PRODUCT_LIST_REQUEST = 'PRODUCT_LIST_REQUEST'
 export const PRODUCT_LIST_SUCCESS = 'PRODUCT_LIST_SUCCESS'
-export const PRODUCT_LIST_FAILURE = 'PRODUCT_LIST_FAILURE'
+export const PRODUCT_LIST_FAIL = 'PRODUCT_LIST_FAIL'
 
 
 /* These three req. pertain to acquiring specific details on selected products */
 export const PRODUCT_DETAILS_REQUEST = 'PRODUCT_DETAILS_REQUEST'
 export const PRODUCT_DETAILS_SUCCESS = 'PRODUCT_DETAILS_SUCCESS'
-export const PRODUCT_DETAILS_FAILURE = 'PRODUCT_DETAILS_FAILURE'
+export const PRODUCT_DETAILS_FAIL = 'PRODUCT_DETAILS_FAIL'
