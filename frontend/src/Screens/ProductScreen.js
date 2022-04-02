825b85e9e06c7e793cb5b32da1d480b66b4a200a
jessica <jessica.vargas@headlight.com>
2022-04-02 10:41:22 -0600

    product screen edits

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, Container } from 'react-bootstrap'
 import ReactHtmlParser from 'react-html-parser'
+import Rating from '../components/Rating'
 
 const ProductScreen = () => {
 
