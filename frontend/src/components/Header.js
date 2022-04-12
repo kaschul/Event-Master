2a3bc0d7c3ec9556fdf13e0d89d05c0ced3a2af9
kaschul <47723396+kaschul@users.noreply.github.com>
2022-04-12 11:52:10 -0700

    update header, store, server, userModel

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,29 +1,55 @@
 import React from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+
+  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+  
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
       <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
           <Container>
+            
             <LinkContainer to="/"> 
               <Navbar.Brand>Event Master</Navbar.Brand>
             </LinkContainer>
+            
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <LinkContainer to="/Basket of Goodies">
+            
+                <LinkContainer to="/cart">
                   <Nav.Link><i className='fa-solid fa-basket-shopping'></i> Basket of Goodies</Nav.Link>
                 </LinkContainer>
-                <LinkContainer to="/Portal to Account"> 
-                  <Nav.Link><i className='fa-solid fa-bullseye'></i> Portal to Account</Nav.Link>
-                </LinkContainer> 
+                  
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Portal to Account</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <LinkContainer to='/login'>
+                    <Nav.Link><i className='fa-solid fa-bullseye'></i> Sign In</Nav.Link>
+                  </LinkContainer>
+                )}
+
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        
       </header>
     )
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,26 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { userLoginReducer } from './reducer/userReducers'
+
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems')
-    ? JSON.parse(localStorage.getItem('cartItems')) 
-    : []
-const initialState = {cart: {cartItems: cartItemsFromStorage}}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart: {cartItems: cartItemsFromStorage},
+    userLogin: {userInfo: userInfoFromStorage}
+}
+
 const middleware = [thunk]
 const store = createStore(
                 reducer,
