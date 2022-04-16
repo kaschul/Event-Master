8823cd843334f89dcb884ddc0671a11467162ff5
kaschul <47723396+kaschul@users.noreply.github.com>
2022-04-15 17:35:30 -0700

    typo

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
-export const addToCart = (id, qty) => async (disptach, getState) => {
+export const addToCart = (id, qty) => async (dispatch, getState) => {
   const {data} = await axios.get(`/api/products/${id}`)
 
-  disptach({
+  dispatch({
     type: CART_ADD_ITEM,
     payload: {
       product: data._id,
