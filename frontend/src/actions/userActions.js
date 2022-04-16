b8a71f3f5b784289a4d98d7a5cbbfdc36bbb5c21
kaschul <47723396+kaschul@users.noreply.github.com>
2022-04-16 09:05:51 -0700

    typo fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -144,7 +144,7 @@ export const login = (email, password) => async (dispatch)=> {                  
           },
         }
 
-        const {data} = await axios.put(`/api/users/profile, user, config`)
+        const {data} = await axios.put(`/api/users/profile`, user, config)
 
         dispatch({
           type: USER_UPDATE_PROFILE_SUCCESS,
