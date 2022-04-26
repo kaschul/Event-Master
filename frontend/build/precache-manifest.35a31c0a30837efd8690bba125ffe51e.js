cc10ee3fda563d6452f306abaacadd04677fc880
kaschul <47723396+kaschul@users.noreply.github.com>
2022-04-26 16:10:17 -0700

    frontend run build

---
+++ frontend/build/precache-manifest.35a31c0a30837efd8690bba125ffe51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bfb31164a29dce5e56e",
+    "url": "/static/css/main.2c712e2e.chunk.css"
+  },
+  {
+    "revision": "6bfb31164a29dce5e56e",
+    "url": "/static/js/main.dd86898c.chunk.js"
+  },
+  {
+    "revision": "56a6b955b538405f9263",
+    "url": "/static/js/runtime~main.378591ef.js"
+  },
+  {
+    "revision": "9b825b53b6e68bb25e2b",
+    "url": "/static/js/2.0da64166.chunk.js"
+  },
+  {
+    "revision": "e6590171a3439d9b16fe",
+    "url": "/static/js/3.bc83d220.chunk.js"
+  },
+  {
+    "revision": "faf9989271ecb24a370db72a1eb1e5a3",
+    "url": "/index.html"
+  }
+];
