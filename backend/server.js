e191dfeef7e8118df6fc7b349bfe18b3da29eabc
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-04-27 08:16:32 -0700

    correction

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production'){
@@ -29,15 +30,12 @@ if (process.env.NODE_ENV === 'production'){
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
-} else {
+}else {
   app.get('/', (req, res) => {
     res.send('API is running....')
   })
 }
-
 app.use(notFound)
 app.use(errorHandler)
-
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
- 
