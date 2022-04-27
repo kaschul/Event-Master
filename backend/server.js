e7c84ca6f6c74db1e2796a42f1c717080137e3f1
kaschul <47723396+kaschul@users.noreply.github.com>
2022-04-27 10:11:55 -0700

    test

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import productRoutes from './routes/productRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'  
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+
 const app = express()
 dotenv.config()
 connectDB()    
@@ -35,7 +37,5 @@ if (process.env.NODE_ENV === 'production'){
 }
 app.use(notFound)
 app.use(errorHandler)
-
-
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
