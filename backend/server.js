97d6600c517ce8b783f221214ddcc5b4e6a3474d
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-04-27 08:36:27 -0700

    corrections

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import productRoutes from './routes/productRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 
 const app = express()
@@ -37,5 +37,7 @@ if (process.env.NODE_ENV === 'production'){
 }
 app.use(notFound)
 app.use(errorHandler)
+
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
